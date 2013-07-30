a4a53292946fbe2ca317a6894cf765516b19fa62
Eli Rose <erose@oberlin.edu>
2013-07-30 19:44:07 -0400

    Added common_words_from_file to utils.py

---
+++ smartstash/input/utils.py
@@ -0,0 +1,15 @@
+#readInTest.py
+import string
+#We want to eliminate all punctuation except single quotes.
+#This isn't the only case (sometimes you have single quotes around a word, which we do want to get rid of)
+#TODO: a regex that will do this better than the current method (python's string.punctuation without the single quote)
+#TODO: find a list of stopwords, don't count them
+#nltk?
+
+def common_words_from_file(textString, n, stopWordsList = stopWordsList):
+    wordList = textString.split()
+    sanitizedWordList = map(lambda word: ''.join([c for c in word if c not in string.punctuation or c == "\'"]), wordList)
+    wordCounts = {}
+    for word in wordList:
+        wordCounts[word] = wordCounts.get(word, 0) + 1
+    return sorted(wordCounts, key = wordCounts.get)[0:n]
