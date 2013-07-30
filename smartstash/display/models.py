d4296d52ae65290b3f1f8ef12e6e42b2c0d02c3b
rlskoeser <rebecca.s.koeser@emory.edu>
2013-07-30 19:23:12 -0400

    prelim common display item

--- smartstash/display/models.py
+++ smartstash/display/models.py
@@ -3,8 +3,36 @@ from django.db import models
 # Create your models here.
 
 
-# common result item
+class DisplayItem(object):
+    # common item to be used for display
+    # results from various apis should be mapped
+    # to this common api
 
+    title = None
+    # title or label
+    format = None
+    # format or type (image, text, etc)
+    source = None
+    # owning institution or provider
+    date = None
+    # creation date / time period
+    url = None
+    # link out to original item
+    location = None
+    # geographic location
+    thumbnail = None
+    # url to thumbnail image
+
+    def __init__(self, **kwargs):
+        for key, val in kwargs.iteritems():
+            setattr(self, key, val)
+
+    def __repr__(self):
+        return '<%s %s>' % (self.title, self.thumbnail or '<no url>')
+
+
+
+# common result item
 '''
 dpla results:
 
