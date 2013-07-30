f1b2c2b9d59680598e9c51d6e448ae6cb54d13a3
rlskoeser <rebecca.s.koeser@emory.edu>
2013-07-30 16:34:37 -0400

    notes about common item fields

--- smartstash/display/models.py
+++ smartstash/display/models.py
@@ -4,3 +4,31 @@ from django.db import models
 
 
 # common result item
+
+'''
+dpla results:
+
+format type: image text, moving image sound, physical object
+owning institution; also has provider
+label/title
+creator
+description
+search results have link within dpla, link out to partner institution
+rights
+url
+created date
+location
+
+'''
+
+'''
+europeana
+title, alt. title
+description
+geographic coverage
+time period
+types (format) - e.g. image
+url
+source, provider
+
+'''
