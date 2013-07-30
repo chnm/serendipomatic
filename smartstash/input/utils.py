5e63d8df64b70d4c4583b5194fcf637bd474300f
Eli Rose <erose@oberlin.edu>
2013-07-30 19:54:38 -0400

    fixed reverse problem, made name correct

--- smartstash/input/utils.py
+++ smartstash/input/utils.py
@@ -5,10 +5,10 @@ import string
 #TODO: find a list of stopwords, don't count them
 #nltk?
 
-def common_words_from_file(textString, n):
+def common_words(textString, n):
     wordList = textString.split()
     sanitizedWordList = map(lambda word: ''.join([c for c in word if c not in string.punctuation or c == "\'"]), wordList)
     wordCounts = {}
     for word in wordList:
         wordCounts[word] = wordCounts.get(word, 0) + 1
-    return sorted(wordCounts, key = wordCounts.get)[0:n]
+    return sorted(wordCounts, key = wordCounts.get, reverse = True)[0:n]
