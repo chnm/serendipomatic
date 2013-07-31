d8f8d4a9071ddf3cef2152b144738c1a014c4b8c
Scott Kleinman <scott.kleinman@csun.edu>
2013-07-31 09:30:19 -0400

    JQuery and Isotope moved to sitemedia folders; new file for views js scripts

---
+++ sitemedia/js/scripts_view.js
@@ -0,0 +1,16 @@
+var $container = $('#container')
+// initialize Isotope
+$container.isotope({
+  // options...
+  resizable: false, // disable normal resizing
+  // set columnWidth to a percentage of container width
+  masonry: { columnWidth: $container.width() / 5 }
+});
+
+// update columnWidth on window resize
+$(window).smartresize(function(){
+  $container.isotope({
+    // update columnWidth to a percentage of container width
+    masonry: { columnWidth: $container.width() / 5 }
+  });
+});
