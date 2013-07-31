233ad8055257f265e1c73a4464f3f7c0d1cb2a8a
rlskoeser <rebecca.s.koeser@emory.edu>
2013-07-31 09:46:56 -0400

    refactor code into a single django app

--- smartstash/core/views.py
+++ smartstash/core/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
 from django.shortcuts import render
 
-from smartstash.input.forms import InputForm
-from smartstash.input.utils import common_words
-from smartstash.find.models import DPLA
+from smartstash.core.forms import InputForm
+from smartstash.core.utils import common_words
+from smartstash.core.api import DPLA
 
 
 def site_index(request):
@@ -23,18 +23,19 @@ def site_index(request):
             # actual logic here - infer search terms, query apis, display stuff
             text = form.cleaned_data['text']
             search_terms = common_words(text, 15)
-            return items_to_display(request, search_terms)
+            # FIXME: redirect to new page? how to pass search terms
+            return view_items(request, search_terms)
 
         # if not valid: pass through and redisplay errors
 
-    return render(request, 'input/site_index.html',
+    return render(request, 'core/site_index.html',
                   {'input_form': form})
 
 
 # TODO: view copied from display.views, code probably needs to be refactored
 # into a single app
 
-def items_to_display(request, search_terms):
+def view_items(request, search_terms):
     items = DPLA.find_items(**search_terms)
-    return render(request, 'display/view.html',
+    return render(request, 'core/view.html',
                   {'items': items, 'query_terms': search_terms})
--- smartstash/settings.py
+++ smartstash/settings.py
@@ -99,9 +99,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'smartstash.input',
-    'smartstash.find',
-    'smartstash.display'
+    'smartstash.core',
 )
 
 
