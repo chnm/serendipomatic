29e3dee2b1d41c47c7600957d739113295207063
Scott Kleinman <scott.kleinman@csun.edu>
2013-07-30 22:19:33 -0400

    Query terms are passed to the template

--- smartstash/display/views.py
+++ smartstash/display/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from smartstash.find.models import DPLA
 
 def view_results(request):
-	items = DPLA.find_items(['dog', 'cat'])
-	return render(request, 'display/view.html',
-		{'items': items})
+    query_terms = ['dog', 'cat']
+    items = DPLA.find_items(query_terms)
+    return render(request, 'display/view.html',
+        {'items': items, 'query_terms': ', '.join(query_terms)})
