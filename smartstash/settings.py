690dba6697af2e5f600cee5463d46ea08f08c886
rlskoeser <rebecca.s.koeser@emory.edu>
2013-08-01 18:23:21 -0400

    fix db config

--- smartstash/settings.py
+++ smartstash/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES{'default':  dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
