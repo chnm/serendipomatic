1de3f44930b91100b99310b67c4a6967f9e0df37
rlskoeser <rebecca.s.koeser@emory.edu>
2013-08-01 15:46:15 -0400

    simple db model for zotero user info

--- smartstash/auth/models.py
+++ smartstash/auth/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ZoteroUser(models.Model):
+    # username, userid, token
+    username = models.CharField(max_length=256, unique=True)
+    userid = models.CharField(max_length=256, unique=True)
+    token = models.CharField(max_length=256)
+
+    # TODO: __unicode__ method (username?)
+
+    def __repr__(self):
+        return '<ZoteroUser %s>' % self.username
+
+
+# examples code
+# from smartstash.auth.models import ZoteroUser
+#
+# find specific user by username:
+# ZoteroUser.objects.get(username='rlskoeser')
+# raises does not exist exception: https://docs.djangoproject.com/en/1.2/ref/exceptions/#objectdoesnotexist-and-doesnotexist
+# create a new one:
+# zu = ZoteroUser(username='rlskoeser')
+# zu.save()
