16748da7f7a21fc34559a8ede7672c2f880bba0b
Scott Kleinman <scott.kleinman@csun.edu>
2013-08-01 14:45:45 -0400

    Save function and dummy static pages added

--- smartstash/urls.py
+++ smartstash/urls.py
@@ -12,11 +12,18 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.site_index, name='site-index'),
     url(r'^stash/$', views.view_items, name='view-stash'),
+    url(r'^dummy1/$', views.dummy1, name='dummy1'),
+    url(r'^dummy2/$', views.dummy2, name='dummy2'),
+    url(r'^dummy3/$', views.dummy3, name='dummy3'),
+    url(r'^saveme/$', views.saveme, name='saveme'),
 
     # static site content pages
-    url(r'^about/', TemplateView.as_view(template_name='about.html'),
-        name='about'),
-
+    url(r'^dummy1/', TemplateView.as_view(template_name='dummy1.html'),
+        name='dummy1'),
+    url(r'^dummy2/', TemplateView.as_view(template_name='dummy2.html'),
+        name='dummy2'),
+    url(r'^dummy3/', TemplateView.as_view(template_name='dummy3.html'),
+        name='dummy3'),
     # examples
     # url(r'^input/', include('smartstash.input.urls',
     #     namespace='input')
