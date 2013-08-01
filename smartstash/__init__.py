ceb776e2dd4aa2e3a86db1fc0cd079b1368df2c9
rlskoeser <rebecca.s.koeser@emory.edu>
2013-07-31 21:43:01 -0400

    set up software app version & make available to templates for display in footer

--- smartstash/__init__.py
+++ smartstash/__init__.py
@@ -1,7 +1,13 @@
-'''
+__version_info__ = (0, 1, 0, 'dev')
+# software version: set last term to 'dev', 'pre', or None for final release
 
-django app to handle querying various apis and
-(may not have an user-facing views....)
+# Dot-connect all but the last. Last is dash-connected if not None.
+__version__ = '.'.join(str(i) for i in __version_info__[:-1])
+if __version_info__[-1] is not None:
+    __version__ += ('-%s' % (__version_info__[-1],))
 
 
-'''
+def version(request):
+    return {
+        'SW_VERSION': __version__
+    }
