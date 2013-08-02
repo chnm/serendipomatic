a4a1a9b522311b2baa021a940eaf1b550a183a4b
rlskoeser <rebecca.s.koeser@emory.edu>
2013-08-01 23:54:01 -0400

    Revert "Merge branch 'master' into heroku" - undo accidental heroku merge
    This reverts commit da2d32fb3ae4c1656a3f245ec2efd4c61f4ecfde, reversing
    changes made to 86c4ce26450b0b88e7430846b6c6a1429f605732.

--- smartstash/core/forms.py
+++ smartstash/core/forms.py
@@ -6,16 +6,27 @@ class InputForm(forms.Form):
     text = forms.CharField(
         label='Copy and paste some text from an article.',
         help_text='Cut and paste a block of text. [Is this something that should go in a tooltip?]',
-        widget=forms.Textarea(attrs={'tabindex': 4,}),
+        widget=forms.Textarea(attrs={'tabindex': 4}),
         required=False)
-        
+
     zotero_user = forms.CharField(
         label='Enter your Zotero username.',
-        help_text="Enter your Zotero username.",
-        widget=forms.TextInput(attrs={'tabindex': 5,}),
+        help_text="What kind of a loser doesn't know what Zotero is?",
+        widget=forms.TextInput(attrs={'tabindex': 5}),
         required=False)
 
     # TODO: alternate inputs- zotero url?
     # use urlfield if appropriate
     # make text input not required, but add form validation
     # so at least one input is required for form to be valid
+
+    def clean(self):
+        cleaned_data = super(InputForm, self).clean()
+        text = cleaned_data.get('text', None)
+        zotero_user = cleaned_data.get('zotero_user', None)
+
+        if not any([text, zotero_user]):
+            raise forms.ValidationError('Please enter text or a zotero username')
+
+        # Always return the full collection of cleaned data.
+        return cleaned_data
