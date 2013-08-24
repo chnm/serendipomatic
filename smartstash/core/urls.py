2539d08f20861e9c2c1599e0a8fbce45666efcdb
rlskoeser <rebecca.s.koeser@emory.edu>
2013-08-24 14:43:32 -0400

    urls for core module split out

---
+++ smartstash/core/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+from smartstash.core import views
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.view_items, name='view'),
+    url(r'^/list/$', views.save_list, name='list'),
+)
+
