9d42fab877b5197b0d73a53008acf57079d61d0d
rlskoeser <rebecca.s.koeser@emory.edu>
2013-10-18 15:03:19 -0400

    remove extra slash in url for list of discoveries to save #131

--- smartstash/core/urls.py
+++ smartstash/core/urls.py
@@ -5,6 +5,6 @@ from smartstash.core import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.view_items, name='view'),
-    url(r'^/list/$', views.save_list, name='list'),
+    url(r'^list/$', views.save_list, name='list'),
 )
 
